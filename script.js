2408d13025cb863c0122034c1d97c35acaf7a27c
JRSkates <skatesproduction@gmail.com>
2024-10-17 15:57:22 +0100

    counter toggle project

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Write your code here
+const lightbulbs = document.querySelectorAll('.lightbulb');
+
+let clickCount = 0;
+
+lightbulbs.forEach(lightbulb => {
+    lightbulb.addEventListener('click', () => {
+        lightbulb.classList.toggle('active');
+        
+        clickCount += 1;
+
+        const count = document.querySelector('.subtitle');
+        count.textContent = `You've clicked the lights ${clickCount} times`;
+    });
+});
